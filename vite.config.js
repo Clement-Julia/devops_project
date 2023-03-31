1e4111550b39836ceb91c70f5824f1f8e75554b6
Clement-Julia <81628168+Clement-Julia@users.noreply.github.com>
2023-03-31 17:02:56 +0200

    fix: fix github action exclusion

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   test: {
     environment: 'jsdom',
-    exclude: ['main.js', 'test/*', 'test-playwright/*'],
+    exclude: ['main.js', 'test-playwright/*'],
   },
   server: {
     watch: {
